94637f3be294562f6fe456d3b2dc1c11ba0da008
Pierre-Louis BOIDIN <pierre-louisboidin@Pierre-Louiss-MacBook-Pro.local>
2022-12-19 22:12:07 +0100

    CPP5/ex01

---
+++ CPP05/ex01/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef	BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const Bureaucrat &src);
+		~Bureaucrat();
+
+		Bureaucrat			&operator=(const Bureaucrat &src);
+		const std::string	getName() const;
+		int					getGrade() const;
+		void				doIncrement();
+		void				doDecrement();
+		void				signForm(Form &form);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+
+#endif
